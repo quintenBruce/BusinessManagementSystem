566d05d43fab954b099f9dcb7ee9e4fbd93d1b78
Quinten Bruce <qcbruce7@gmail.com>
2022-09-30 00:19:36 -0500

    further revise text area

--- InventoryManagementSystem/wwwroot/js/site.js
+++ InventoryManagementSystem/wwwroot/js/site.js
@@ -3,6 +3,19 @@ event listener on search by name input form to check if the input
 value is a valid search string
 */
 
+$(document).ready(function () {
+    console.log("ready!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+    $('textarea').on('change keyup paste cut', function () {
+        if (/Mobi|Android/i.test(navigator.userAgent)) {
+            $(this).height(0).height(this.scrollHeight);
+        }
+    }).find('textarea').trigger("change");
+
+});
+
+
+
 $("#search-by-name-form").submit(function (event) {
     if ($("#search-by-name-input").val().trim() == "" || $("#search-by-name-input").val().trim() == null || $("#search-by-name-input").val().trim() == " ") {
         event.preventDefault()
