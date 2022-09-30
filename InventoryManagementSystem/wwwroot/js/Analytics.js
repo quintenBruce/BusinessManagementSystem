7c866d9363a6bef5a95b53cd993ced650e769119
Quinten Bruce <qcbruce7@gmail.com>
2022-09-30 00:11:14 -0500

    text area revision

--- InventoryManagementSystem/wwwroot/js/Analytics.js
+++ InventoryManagementSystem/wwwroot/js/Analytics.js
@@ -1,6 +1,6 @@
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
+	return new bootstrap.Popover(popoverTriggerEl)
 })
 
 let revenueByMonth = document.getElementById("revenue-by-month").getContext('2d')
@@ -13,256 +13,256 @@ let engagedUsersXordersPlaced2 = document.getElementById("engaged-usersXorders-p
 console.log(engagedUsersXordersPlaced)
 
 let engagedUsersXordersPlacedChart2 = new Chart(engagedUsersXordersPlaced2, {
-    type: 'line',
-    data: {
-        labels: MonthList,
-        datasets: [
-            {
-                label: "Engaged Users",
-                backgroundColor: "#3b82ec",
+	type: 'line',
+	data: {
+		labels: MonthList,
+		datasets: [
+			{
+				label: "Engaged Users",
+				backgroundColor: "#3b82ec",
 
-                borderWidth: 1,
-                data: MonthEngagedUsers,
-                borderRadius: 100,
-                borderWidth: 3
-            }
+				borderWidth: 1,
+				data: MonthEngagedUsers,
+				borderRadius: 100,
+				borderWidth: 3
+			}
 
-        ]
-    },
-    options: {
-        pointRadius: 5,
-        tension: 0.3,
-        scales: {
-            y: {
-                title: {
-                    display: true,
-                    text: "# Orders",
-                },
-                grid: {
-                    display: false
-                }
-            },
+		]
+	},
+	options: {
+		pointRadius: 5,
+		tension: 0.3,
+		scales: {
+			y: {
+				title: {
+					display: true,
+					text: "# Orders",
+				},
+				grid: {
+					display: false
+				}
+			},
 
-            x: {
-                title: {
-                    display: true,
-                    text: "Month",
-                },
-                grid: {
-                    display: false
-                }
-            }
-        },
-        plugins: {
-            legend: {
-                labels: {
-                    usePointStyle: true,
-                    padding: 20
-                },
-            }
-        }
-    }
+			x: {
+				title: {
+					display: true,
+					text: "Month",
+				},
+				grid: {
+					display: false
+				}
+			}
+		},
+		plugins: {
+			legend: {
+				labels: {
+					usePointStyle: true,
+					padding: 20
+				},
+			}
+		}
+	}
 });
 
 let engagedUsersXordersPlacedChart = new Chart(engagedUsersXordersPlaced, {
-    type: 'line',
-    data: {
-        labels: MonthList,
-        datasets: [
-            {
-                label: "Engaged Users",
-                backgroundColor: "#3b82ec",
+	type: 'line',
+	data: {
+		labels: MonthList,
+		datasets: [
+			{
+				label: "Engaged Users",
+				backgroundColor: "#3b82ec",
 
-                borderWidth: 1,
-                data: MonthEngagedUsers,
-                borderRadius: 100,
-                borderWidth: 3
-            }
+				borderWidth: 1,
+				data: MonthEngagedUsers,
+				borderRadius: 100,
+				borderWidth: 3
+			}
 
-        ]
-    },
-    options: {
-        pointRadius: 5,
-        tension: 0.3,
-        scales: {
-            y: {
-                title: {
-                    display: true,
-                    text: "# Orders",
-                },
-                grid: {
-                    display: false
-                }
-            },
+		]
+	},
+	options: {
+		pointRadius: 5,
+		tension: 0.3,
+		scales: {
+			y: {
+				title: {
+					display: true,
+					text: "# Orders",
+				},
+				grid: {
+					display: false
+				}
+			},
 
-            x: {
-                title: {
-                    display: true,
-                    text: "Month",
-                },
-                grid: {
-                    display: false
-                }
-            }
-        },
-        plugins: {
-            legend: {
-                labels: {
-                    usePointStyle: true,
-                    padding: 20
-                },
-            }
-        }
-    }
+			x: {
+				title: {
+					display: true,
+					text: "Month",
+				},
+				grid: {
+					display: false
+				}
+			}
+		},
+		plugins: {
+			legend: {
+				labels: {
+					usePointStyle: true,
+					padding: 20
+				},
+			}
+		}
+	}
 });
 
 let revenueByMonthChart = new Chart(revenueByMonth, {
-    type: 'bar',
-    data: {
-        labels: MonthList,
-        datasets: [
+	type: 'bar',
+	data: {
+		labels: MonthList,
+		datasets: [
 
-            {
-                label: 'Total Revenue',
-                data: MonthRevenueList,
-                backgroundColor: "#3b82ec",
-                borderRadius: 100,
-                barPercentage: .5
-            }
-        ]
-    },
-    options: {
-        scales: {
-            y: {
-                stacked: true,
-                title: {
-                    display: true,
-                    text: "Revenue ($)",
-                },
-                grid: {
-                    display: false
-                }
-            },
+			{
+				label: 'Total Revenue',
+				data: MonthRevenueList,
+				backgroundColor: "#3b82ec",
+				borderRadius: 100,
+				barPercentage: .5
+			}
+		]
+	},
+	options: {
+		scales: {
+			y: {
+				stacked: true,
+				title: {
+					display: true,
+					text: "Revenue ($)",
+				},
+				grid: {
+					display: false
+				}
+			},
 
-            x: {
-                stacked: true,
-                title: {
-                    display: true,
-                    text: "Month",
-                },
-                grid: {
-                    display: false
-                }
-            }
-        },
-        plugins: {
-            legend: {
-                labels: {
-                    usePointStyle: true
-                },
-            }
-        }
-    }
+			x: {
+				stacked: true,
+				title: {
+					display: true,
+					text: "Month",
+				},
+				grid: {
+					display: false
+				}
+			}
+		},
+		plugins: {
+			legend: {
+				labels: {
+					usePointStyle: true
+				},
+			}
+		}
+	}
 });
 
 let ordersByMonthChart = new Chart(ordersByMonth, {
-    type: 'bar',
-    data: {
-        labels: MonthList,
-        datasets: [
-            {
-                label: "Orders Placed",
-                backgroundColor: "#3b82ec",
+	type: 'bar',
+	data: {
+		labels: MonthList,
+		datasets: [
+			{
+				label: "Orders Placed",
+				backgroundColor: "#3b82ec",
 
-                borderWidth: 1,
-                data: MonthTotalOrdersPlaced,
-                borderRadius: 100
-            },
-            {
-                label: "Orders Completed (On Time)",
-                backgroundColor: "#66c2a5",
+				borderWidth: 1,
+				data: MonthTotalOrdersPlaced,
+				borderRadius: 100
+			},
+			{
+				label: "Orders Completed (On Time)",
+				backgroundColor: "#66c2a5",
 
-                borderWidth: 1,
-                data: MonthOnTimeOrderList,
-                stack: "stack 0",
-                borderRadius: 100
-            },
-            {
-                label: "Orders Completed (Past Due)",
-                backgroundColor: "#f46d43",
+				borderWidth: 1,
+				data: MonthOnTimeOrderList,
+				stack: "stack 0",
+				borderRadius: 100
+			},
+			{
+				label: "Orders Completed (Past Due)",
+				backgroundColor: "#f46d43",
 
-                borderWidth: 1,
-                data: MonthPastDueOrderList,
-                stack: "stack 0",
-                borderRadius: 100
-            }
-        ]
-    },
-    options: {
-        scales: {
-            y: {
-                title: {
-                    display: true,
-                    text: "# Orders",
-                },
-                grid: {
-                    display: false
-                }
-            },
+				borderWidth: 1,
+				data: MonthPastDueOrderList,
+				stack: "stack 0",
+				borderRadius: 100
+			}
+		]
+	},
+	options: {
+		scales: {
+			y: {
+				title: {
+					display: true,
+					text: "# Orders",
+				},
+				grid: {
+					display: false
+				}
+			},
 
-            x: {
-                title: {
-                    display: true,
-                    text: "Month",
-                },
-                grid: {
-                    display: false
-                }
-            }
-        },
-        plugins: {
-            legend: {
-                labels: {
-                    usePointStyle: true,
-                    padding: 20
-                },
-            }
-        }
-    }
+			x: {
+				title: {
+					display: true,
+					text: "Month",
+				},
+				grid: {
+					display: false
+				}
+			}
+		},
+		plugins: {
+			legend: {
+				labels: {
+					usePointStyle: true,
+					padding: 20
+				},
+			}
+		}
+	}
 });
 
 let revenueByCategoryChart = new Chart(revenueByCategory, {
-    type: 'doughnut',
-    data: {
-        labels: categoryNamesList,
-        datasets: [
-            {
-                label: 'Orders by Category',
-                data: categoryOrderCount,
-                backgroundColor: [
+	type: 'doughnut',
+	data: {
+		labels: categoryNamesList,
+		datasets: [
+			{
+				label: 'Orders by Category',
+				data: categoryOrderCount,
+				backgroundColor: [
 
-                    "#3288bd",
-                    "#66c2a5",
-                    "#abdda4",
-                    "#e6f598",
-                    "#fee08b",
-                    "#fdae61",
-                    "#f46d43",
-                    "#d53e4f",
+					"#3288bd",
+					"#66c2a5",
+					"#abdda4",
+					"#e6f598",
+					"#fee08b",
+					"#fdae61",
+					"#f46d43",
+					"#d53e4f",
 
-                ],
-                barPercentage: .5
-            }
+				],
+				barPercentage: .5
+			}
 
-        ]
-    },
-    options: {
-        plugins: {
-            legend: {
-                display: false
-            }
-        },
-        cutout: "75%",
-        aspectRatio: 2
-    }
+		]
+	},
+	options: {
+		plugins: {
+			legend: {
+				display: false
+			}
+		},
+		cutout: "75%",
+		aspectRatio: 2
+	}
 });
