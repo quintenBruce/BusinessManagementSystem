b90b40157905c78298b3c1c760ef3e1e5c913a21
Quinten Bruce <qcbruce7@gmail.com>
2022-10-01 12:41:20 -0500

    fixed AB#74

--- InventoryManagementSystem/Services/CalenderService.cs
+++ InventoryManagementSystem/Services/CalenderService.cs
@@ -5,6 +5,12 @@ namespace InventoryManagementSystem.Services
 {
     public class CalenderService : ICalender
     {
+        private readonly IConfiguration _configuration;
+        public CalenderService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public List<CalenderViewModel> GetCalenderModel(IEnumerable<CalenderDTO> calenderDTOs)
         {
             List<CalenderViewModel> viewModel = new List<CalenderViewModel>();
@@ -14,7 +20,7 @@ namespace InventoryManagementSystem.Services
                 string title = calenderDTO.NumProducts.ToString();
                 title = (title == "1") ? (title + " Product") : (title + " Products");
                 string start = calenderDTO.FulfillmentDate.ToString("yyyy/MM/dd").Replace("/", "-");
-                string url = "https://localhost:7020/Order/OrderDetails/" + calenderDTO.Id.ToString();
+                string url = _configuration.GetValue<string>("RootAdress") + "/Order/OrderDetails/" + calenderDTO.Id.ToString();
                 viewModel.Add(new(title, start, "#3788d8", "#3788d8", url));
             }
             return viewModel;
